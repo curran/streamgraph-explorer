12a27b5bc9a2f2dd9c89b39870957f61def58f8f
curran <curran.kelleher@gmail.com>
2017-08-15 12:51:41 +0530

    Compute data by src

--- src/dataFlow.js
+++ src/dataFlow.js
@@ -1,5 +1,7 @@
 import ReactiveModel from 'reactive-model';
 import unpackData from './unpackData';
+import { nest } from 'd3-collection';
+import { sum } from 'd3-array';
 
 const dataFlow = ReactiveModel();
 
@@ -9,6 +11,21 @@ dataFlow('packedData');
 // Reactive functions.
 dataFlow('data', unpackData, 'packedData');
 
-dataFlow(data => console.log(data[0]), 'data');
+// TODO filter by selected types, origin, destination
+dataFlow('dataFiltered', d => d, 'data');
+
+dataFlow('dataBySrc', dataFiltered => {
+  return nest()
+    .key(function (d) { return d.src; })
+    .key(function (d) { return d.year; })
+    .rollup(function(values) {
+      return sum(values, function(d) {
+        return d.Value;
+      })
+    })
+    .entries(dataFiltered);
+}, 'dataFiltered');
+
+dataFlow(data => console.log(data[0]), 'dataBySrc');
 
 export default dataFlow;
