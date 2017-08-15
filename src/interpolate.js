2e44862a282c7dbc32a1ec780b0b35bebd936d5e
curran <curran.kelleher@gmail.com>
2017-08-16 02:14:37 +0530

    Use zero for interpolation

--- src/interpolate.js
+++ src/interpolate.js
@@ -3,15 +3,15 @@ import { bisector } from 'd3-array';
 const bisectDate = bisector(d => d.date).left;
 
 const interpolateValue = (values, date) => {
-  const i = bisectDate(values, date, 0, values.length - 1);
-  if (i > 0) {
+  const i = bisectDate(values, date);
+  if (i > 0 && i < values.length) {
     const a = values[i - 1];
     const b = values[i];
     const t = (date - a.date) / (b.date - a.date);
     return a.value * (1 - t) + b.value * t;
   }
   // TODO consider carefully on how to handle edge cases
-  return values[i].value;
+  return 0;// values[i].value;
 };
 
 // Interpolate values, create data structure for d3.stack.
