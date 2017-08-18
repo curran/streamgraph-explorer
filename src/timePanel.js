b53447d63e1d7923750f8fb09689ea19b339be5e
curran <curran.kelleher@gmail.com>
2017-08-18 14:55:44 +0530

    Center axis vertically

--- src/timePanel.js
+++ src/timePanel.js
@@ -2,18 +2,20 @@ import { scaleTime } from 'd3-scale';
 import { axisBottom } from 'd3-axis';
 
 const xScale = scaleTime();
-const xAxis = axisBottom().scale(xScale);
+const xAxis = axisBottom()
+  .scale(xScale)
+  .tickSize(0)
+  .tickPadding(-3);
 
 const TimePanel = g => (box, margin, timeExtent) => {
   const innerWidth = box.width - margin.right - margin.left;
-  const innerHeight = box.height - margin.top - margin.bottom;
 
   xScale
     .domain(timeExtent)
     .range([0, innerWidth]);
   
   const translateX = box.x + margin.left;
-  const translateY = box.y + margin.top;
+  const translateY = box.y + box.height / 2;
   g
       .attr('transform', `translate(${translateX},${translateY})`)
       .call(xAxis);
